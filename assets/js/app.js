7a0d661e767d653d02748b07ed548ae4be38da6e
Tim Dolan <timmydolan21@gmail.com>
2018-09-11 13:43:28 -0400

    GiphyAssignment

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+//click listener for all buttons
+$(".animal").on("click", function() {
+//variable storing the buttons
+var animals = $(this).attr("data-animal");
+var topics = ["dog", "cat", "squirrel", "tiger", "lion"];
+
+    
+  //queryURL using the animal name
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q" +
+    topics +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
+  //AJAX request for information
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    //response function dictating what to do with results
+  }).then(function(response) {
+    console.log(queryURL);
+
+    console.log(response);
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var animalDiv = $("<div>");
+      var p = $("<p>").text();
+    }
+  });
+});
